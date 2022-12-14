1eef37db8f8ab88bab4c4f919a0e9f23fed531a5
Crisgarner <@crisgarner>
2022-12-14 09:42:52 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { OKX } from './OKX';
+import '@rainbow-me/rainbowkit/styles.css';
+import { RainbowKitProvider, connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+    metaMaskWallet,
+    rainbowWallet,
+    walletConnectWallet,
+    coinbaseWallet,
+    argentWallet,
+    ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import App from './App';
+
+const { chains, provider, webSocketProvider } = configureChains(
+    [
+        chain.mainnet,
+        chain.polygon,
+        chain.optimism,
+        chain.arbitrum,
+        ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [chain.goerli] : []),
+    ],
+    [
+        alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID }),
+        publicProvider(),
+    ]
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const connectors = connectorsForWallets([
+    {
+        groupName: 'Recommended',
+        wallets: [
+            metaMaskWallet({ chains }),
+            rainbowWallet({ chains }),
+            walletConnectWallet({ chains }),
+            coinbaseWallet({ chains }),
+            argentWallet({ chains }),
+            ledgerWallet({ chains }),
+            OKX({ chains })
+        ],
+    },
+]);
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+    webSocketProvider,
+});
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
+root.render(
+    <React.StrictMode>
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains}>
+                <App />
+            </RainbowKitProvider>
+        </WagmiConfig>
+    </React.StrictMode>
+);
